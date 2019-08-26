6dd4c2978303f57fbcfac5b90a06a9af3f45cef1
LymeTree <keelymepi99@gmail.com>
2019-08-25 23:49:41 -0400

    First Web Upload v3

---
+++ js/script.js
@@ -0,0 +1,93 @@
+/* global $ */
+/*eslint-env jquery*/
+
+$('document').ready(function() {
+    
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    }  else {
+        $('nav').removeClass('sticky');
+    }
+    }, {
+    offset: '60px;'
+});
+    $('.js--section-bio').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    }  else {
+        $('nav').removeClass('sticky');
+    }
+    }, {
+    offset: '60px;'
+});
+    $('.js--section-purpose').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    }  else {
+        $('nav').removeClass('sticky');
+    }
+    }, {
+    offset: '60px;'
+});
+
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-tops').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-tops').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-tops').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+});    
+                                 
+    /* Mobile nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round'); 
+        }
+        
+    });
+
+
+
+    
+
+
+    
