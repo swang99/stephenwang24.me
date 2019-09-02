89048fed9e765074dafbc9487881955bec026537
LymeTree <keelymepi99@gmail.com>
2019-09-02 08:51:51 -0400

    Edit 1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 /* global $ */
-/*eslint-env jquery*/
+/*eslint-env browser*/
 
 $('document').ready(function() {
-    
     /* For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
     if (direction == "down") {
@@ -41,7 +40,6 @@ $('document').ready(function() {
     $('.js--scroll-to-features').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
-
 
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function() {
@@ -84,6 +82,111 @@ $('document').ready(function() {
         }
         
     });
+
+    /* Modal images */
+
+    var img = document.getElementById("RainChain01");
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("Deck");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("PurpleFlower01");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("RainChain02");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("RainChain03");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("PinkFlower01");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("Sunset01");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("RainChain04");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("RedFlower01");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("Sunset02");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("Landing01");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("YellowLily");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("LeavesBranch");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("Landing02");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    img = document.getElementById("Landing03");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+    
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+      modal.style.display = "none";
+    }
 
 
 
