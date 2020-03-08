216458ac79b96133052c94520c32e796cf2de6c0
LymeTree <keelymepi99@gmail.com>
2020-03-08 12:23:44 -0400

    nav update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /* global $ */
 /*eslint-env browser*/
 
-$('document').ready(function() {
+$('document').ready(function () {
     /* For the sticky navigation */
-    $('.js--section-features').waypoint(function(direction) {
+    $('.js--section-features').waypoint(function (direction) {
     if (direction == "down") {
         $('nav').addClass('sticky');
     }  else {
@@ -15,19 +15,25 @@ $('document').ready(function() {
     $('.js--section-bio').waypoint(function(direction) {
     if (direction == "down") {
         $('nav').addClass('sticky');
-    }  else {
-        $('nav').removeClass('sticky');
     }
-    }, {
+    else {
+        $('nav').fadeOut(250, function() {
+            $(this).removeClass('sticky');
+    });
+        $('nav').fadeIn(250)
+    }}, {
     offset: '60px;'
 });
     $('.js--section-purpose').waypoint(function(direction) {
     if (direction == "down") {
         $('nav').addClass('sticky');
-    }  else {
-        $('nav').removeClass('sticky');
     }
-    }, {
+    else {
+        $('nav').fadeOut(250, function() {
+            $(this).removeClass('sticky');
+    });
+        $('nav').fadeIn(250)
+    }}, {
     offset: '60px;'
 });
 
@@ -42,34 +48,33 @@ $('document').ready(function() {
     });
 
     /* Animations on scroll */
-    $('.js--wp-1').waypoint(function() {
+    $('.js--wp-1').waypoint(function () {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
     
-    $('.js--wp-2').waypoint(function() {
+    $('.js--wp-2').waypoint(function () {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
     });
     
-    $('.js--wp-3').waypoint(function() {
+    $('.js--wp-3').waypoint(function () {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
         offset: '50%'
     });
     
-    $('.js--wp-4').waypoint(function() {
+    $('.js--wp-4').waypoint(function () {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
     });
-    
 });    
                                  
     /* Mobile nav */
-    $('.js--nav-icon').click(function() {
+    $('.js--nav-icon').click(function () {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
         nav.slideToggle(200);
@@ -84,7 +89,6 @@ $('document').ready(function() {
     });
 
     /* Modal images */
-
     var img = document.getElementById("RainChain01");
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("img01");
@@ -179,6 +183,12 @@ $('document').ready(function() {
     }
     
     img = document.getElementById("GoldenWheat");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    
+     img = document.getElementById("DHMCChamber1");
     img.onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
